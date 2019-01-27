9ba39945f2ecd67da668f0499c40f87c7b6ed3f8
mglavall <mgomezlavall@gmail.com>
2019-01-27 20:05:48 +0100

    Finish new card component

--- src/components/NewCard.js
+++ src/components/NewCard.js
@@ -1,13 +1,35 @@
-import React from "react";
-import "./Card.css";
+import React, { Component } from "react";
+import "./NewCard.css";
 
-const card = props => {
-  return (
-    <div className="card col-10">
-      <div>new card</div>
-      <button onClick={props.cardAdded.bind(this, "nueva carta")}>Create card</button>
-    </div>
-  );
-};
+class NewCard extends Component {
+  state = {
+    cardTitle: "Add new card."
+  };
 
-export default card;
+  handleChange(event) {
+    this.setState({ cardTitle: event.target.value });
+  }
+
+  render() {
+    return (
+      <div className="card d-flex flex-col">
+        <textarea
+          type="text"
+          rows="4"
+          maxlength="80"
+          value={this.state.cardTitle}
+          onChange={this.handleChange.bind(this)}
+          name="card"
+        />
+        <button
+          className="add-button"
+          onClick={this.props.cardAdded.bind(this, this.state.cardTitle)}
+        >
+          Create card
+        </button>
+      </div>
+    );
+  }
+}
+
+export default NewCard;
